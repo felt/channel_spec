7a375805fb31d1ea5fa8650299930faf857d4740
Dorgan <dorgandash@gmail.com>
2024-05-08 21:24:49 -0300

    Add LICENSE (#4)
    * Add license
    
    * Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule ChannelSpec.MixProject do
   use Mix.Project
 
+  @repo_url "https://github.com/felt/channel_spec"
+  @version "0.1.1"
+
   def project do
     [
       app: :channel_spec,
-      version: "0.1.1",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -27,6 +32,31 @@ defmodule ChannelSpec.MixProject do
       {:json_xema, "~> 0.6"},
       {:phoenix, "~> 1.7"},
       {:xema, "~> 0.17"}
+    ]
+  end
+
+  defp package do
+    [
+      description:
+        "A Phoenix Channels specification library for automatic data validation and schema generation.",
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @repo_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        LICENSE: [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      homepage_url: @repo_url,
+      source_ref: "v#{@version}",
+      source_url: @repo_url,
+      formatters: ["html"]
     ]
   end
 end
